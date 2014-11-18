1ac94f73aad2ad151569bdb4188929044964024e
Murder-is-Tasty <mb.mattboehm@gmail.com>
2014-11-17 21:03:35 -0500

    Initial Commit
    Start of project.

---
+++ src/main/java/net/sqdmc/murderousmobs/MurderousMobs.java
@@ -0,0 +1,18 @@
+package net.sqdmc.murderousmobs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Matt on 11/17/2014.
+ */
+public class MurderousMobs extends JavaPlugin {
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
